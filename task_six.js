31825781f5745860419244924609e66e1cc135f8
Deborah Okeke <okekedeborah@gmail.com>
2023-06-21 20:46:12 +0100

    finish

---
+++ task_six.js
@@ -0,0 +1,4 @@
+// Compare multiple conditions with logical operators
+
+const b = 5
+b >= 5 && b <= 10 ? console.log('this number is within the range of 5 to 10') : console.log('this number is outside the range of 5 to 10')
