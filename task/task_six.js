95155451427ea3e253ca77f2d35d9d7d5f368433
Deborah Okeke <72860663+debeemedia@users.noreply.github.com>
2023-06-21 21:02:13 +0100

    Delete task directory

--- task/task_six.js
+++
@@ -1,4 +0,0 @@
-// Compare multiple conditions with logical operators
-
-const b = 5
-b >= 5 && b <= 10 ? console.log('this number is within the range of 5 to 10') : console.log('this number is outside the range of 5 to 10')
